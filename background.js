da161388d92e50c0dfa811d213a9d919cddd3ac9
edamame-maru <119129834+edamame-maru@users.noreply.github.com>
2024-05-22 08:42:48 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'connect') {
-      setProxy('139.162.238.184', '26698', sendResponse);
+      setProxy('46.250.242.57', '3128', sendResponse);
       return true; // Keep the message channel open for sendResponse
     } else if (request.action === 'disconnect') {
       clearProxy(sendResponse);
